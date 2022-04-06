57c47c6a350b9ca41956024d482a2d2796b43c4c
DLSG <denilsonsalguz@hotmail.com>
2022-04-06 10:17:53 -0500

    using php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+  $contacts = [
+    ["name" => "Denilson", "phoneNumber"=>"980558364"],
+    ["name" => "Yessica", "phoneNumber"=>"993662890"],
+    ["name" => "Romulo", "phoneNumber"=>"962251308"],
+  ]
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -50,37 +57,18 @@
       <main>
         <div class="container pt-4 p-3">
           <div class="row">
+            <?php foreach($contacts as $contact){?>
             <div class="col-md-4 mb-3">
               <div class="card text-center">
                 <div class="card-body">
-                  <h3 class="card-title text-capitalize">Contact Name 1</h3>
-                  <p class="m-2">987654321</p>
+                  <h3 class="card-title text-capitalize"><?= $contact["name"]?></h3>
+                  <p class="m-2"><?= $contact["phoneNumber"]?></p>
                   <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
                   <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
                 </div>
               </div>
             </div>
-    
-            <div class="col-md-4 mb-3">
-              <div class="card text-center">
-                <div class="card-body">
-                  <h3 class="card-title text-capitalize">Contact Name 2</h3>
-                  <p class="m-2">987654321</p>
-                  <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                  <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-                </div>
-              </div>
-            </div>
-    
-            <div class="col-md-4 mb-3">
-              <div class="card text-center">
-                <div class="card-body">
-                  <h3 class="card-title text-capitalize">Contact Name 3</h3>
-                  <p class="m-2">987654321</p>
-                  <a href="#" class="btn btn-secondary mb-2">Edit Contact</a>
-                  <a href="#" class="btn btn-danger mb-2">Delete Contact</a>
-                </div>
-              </div>
+            <?php } ?>
             </div>
           </div>
         </div>
