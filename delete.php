e02941af35c79f20388eb0770519fa27bf0a973b
DLSG <denilsonsalguz@hotmail.com>
2022-04-11 09:54:51 -0500

    delete data and not found page

---
+++ delete.php
@@ -0,0 +1,18 @@
+<?php
+    require "database.php";
+    $id = $_GET["id"];
+    $lookData = $conection -> prepare("SELECT * FROM contacts WHERE id_contact = :id");
+    $lookData -> execute([":id"=> $id]);
+    
+    if($lookData->rowCount() == 0){
+        http_response_code(404);
+        header("Location: notFound.php");
+        return;
+    }
+
+    $dropContact = $conection ->prepare("DELETE FROM contacts WHERE id_contact = :id");
+    //$dropContact -> bindParam(":id", $id);
+    $dropContact -> execute([":id"=> $id]);
+
+    header("Location: index.php");
+?>
